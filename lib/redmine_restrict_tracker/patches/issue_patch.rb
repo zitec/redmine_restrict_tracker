578a73c4c8f8d4d495fa612d224e96b79dc9c9c8
Codrut Constantin Gusoi <codrut.gusoi@zitec.ro>
2015-04-07 18:36:33 +0300

    Using configuration values in the hook

--- lib/redmine_restrict_tracker/patches/issue_patch.rb
+++ lib/redmine_restrict_tracker/patches/issue_patch.rb
@@ -16,43 +16,35 @@ module RedmineRestrictTracker
         end
 
         def restrict_root
-          if [2,5,6,8,9].include? tracker_id
+          if (Setting.plugin_redmine_restrict_tracker[:root_nodes].split(',')
+              .map(&:to_i).include?(tracker_id))
             true
           else
-            errors.add :base, "#{tracker.name} can't be a root node, please assign a parent."
+            errors.add :base, "#{tracker.name} can't be a root node, please assign a parent!"
             false
           end
         end
 
         def restrict_parent
-          case tracker_id
-          when 1
-            restrict_for [2], 'Bugs can only be children for Features.'
-          when 2
-            restrict_for [5,6], 'Features can only be children for Epics and Releases.'
-          when 3
-            restrict_for [2], 'Supports can only be children for Features.'
-          when 4
-            restrict_for [2], 'Task can only be children for Features.'
-          when 5
-            restrict_for [5], 'Epics can only be children for Epics.'
-          when 6
-            restrict_for [], 'Releases can only be root nodes.'
-          when 7
-            restrict_for [2], 'QA Tasks can only be children for Features.'
-          when 8
-            restrict_for [2,5], 'Enhancements can only be children for Features and Epics.'
-          when 9
-            restrict_for [2], 'Alerts can only be children for Features.'
-          else
+          tracker_name = tracker.name
+          setting_name = "parents_for_#{tracker_name.downcase.split(' ').join('_')}"
+          possible_parent_trackers = Setting.plugin_redmine_restrict_tracker[setting_name]
+            .split(',').map(&:to_i)
+          if (possible_parent_trackers.include?(tracker_id))
             true
+          else
+            possible_parents = Tracker.where(id: possible_parent_trackers).pluck(:name).map(&:pluralize)
+            if possible_parents.size == 0
+              errors.add :base, "#{tracker_name.pluralize} can't be set as children!"
+              return false
+            elsif possible_parents.size > 1
+              parents_string = possible_parents[0..-2].join(', ') << " and " << possible_parents[-1]
+            else
+              parents_string = possible_parents[0]
+            end
+            errors.add :base, "#{tracker_name.pluralize} can only be children of #{parents_string}!"
+            false
           end
-        end
-
-        def restrict_for(tracker_ids, error_message)
-          restriction = tracker_ids.include? Issue.where(id: parent_issue_id).select(:tracker_id).first.tracker_id
-          errors.add(:base, error_message) unless restriction
-          restriction
         end
       end
     end
