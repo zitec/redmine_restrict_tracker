e3390d565362e55e188057113ca6306db76e129d
Codrut Constantin Gusoi <codrut.gusoi@zitec.com>
2015-09-11 12:33:03 +0300

    Added simplecov settings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,16 @@
+# Code coverage setup
+require 'simplecov'
+SimpleCov.start do
+  add_group 'Libraries', 'lib'
+  add_filter '/test/'
+  add_filter 'init.rb'
+  root File.expand_path(File.dirname(__FILE__) + '/../')
+  coverage_dir 'tmp/coverage'
+end
+
 # Load the Redmine helper
 require File.expand_path File.dirname(__FILE__) << '/../../../test/test_helper'
+
 # Load factories
 factories_folder = File.expand_path File.dirname(__FILE__) << '/factories/'
 FactoryGirl.definition_file_paths << factories_folder
