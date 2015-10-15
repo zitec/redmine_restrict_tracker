e86105a54caef6c31666147bc78083076afe461a
Codrut Constantin Gusoi <codrut.gusoi@zitec.com>
2015-10-15 13:19:31 +0300

    Using default factories from redmine_testing_gems

--- spec/factories/enumeration.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :issue_priority do
-    sequence(:name) {|count| "Sample Name #{ count }" }
-    sequence(:position) {|count| count }
-    sequence(:position_name) {|count| "Sample Position Name #{ count }" }
-    type 'IssuePriority'
-    active true
-  end
-end
--- spec/factories/issue_status.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :issue_status do
-    sequence(:name) {|count| "Sample Name #{ count }" }
-    is_closed false
-  end
-end
