0cca15ea36e56484b9d050e104d669b785769217
Codrut Constantin Gusoi <codrut.gusoi@zitec.com>
2015-10-29 17:55:21 +0200

    Changed version to reflect github future tag

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_restrict_tracker do
   name 'Restrict Tracker'
   author 'Zitec'
   description 'Restricts the root and child trackers'
-  version '1.0.0'
+  version '0.2.0'
   url 'https://github.com/ZitecCOM/redmine_restrict_tracker'
   author_url 'http://www.zitec.com'
   requires_redmine version_or_higher: '3.1.1'
